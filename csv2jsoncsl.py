9191a80dad8620418c7fb4d45d5c225f758b9985
juliengossa <julien.gossa@gmail.com>
2020-11-15 16:22:48 +0100

    Premiere exportation réussie

--- csv2jsoncsl.py
+++ csv2jsoncsl.py
@@ -1,14 +1,15 @@
 #!/bin/python3
 import sys
 import csv
+import json
 
 if len(sys.argv) < 2:
     print("Usage: python3 csv2jsoncls.py csvfile")
     sys.exit(1)
 
-with open(sys.argv[1], newline='') as csvfile:
+with open(sys.argv[1], newline='', encoding="utf-8") as csvfile:
     rows = csv.DictReader(csvfile)
-    print(rows.fieldnames)
+    #print(rows.fieldnames)
     rows.fieldnames[0] = "ID"
 
     refs = []
@@ -23,11 +24,11 @@ with open(sys.argv[1], newline='') as csvfile:
             "container-title" : row["publication"],
             "title"           : row["titre"],
             "URL"             : row["url"],
-            "author"          : {"literal":row["auteurs"]},
-            "recipient"       : {"literal":row["destinataire"]},
+            "author"          : [{"literal":row["auteurs"]}],
+            "recipient"       : [{"literal":row["destinataire"]}],
             "issued"          : {"raw":row["date"]},
             "note"            : row["position"]
             }
         refs.append(ref)
 
-print(refs)
+print(json.dumps(refs, indent=4, ensure_ascii=False))
