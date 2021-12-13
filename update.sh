3b1ae3fbbd17dfba310e2d8ae4a2f739c2f79c84
juliengossa <julien.gossa@gmail.com>
2021-12-13 22:34:55 +0100

    Mise à jour des références

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 if [ -z "$1" ]; then
   lastid=`cat positionnements.json | grep '"id"' | tail -1 | sed -r 's/.*-([0-9]*)",/\1/g'`
@@ -17,9 +18,11 @@ python3 depotools.py results-survey435945.csv --import-limesurvey
 python3 depotools.py results-survey435945.csv --zotpress --zotero `cat zotero.key` 2348553 75KBP7II --minid $minid
 python3 depotools.py results-survey435945.csv --storefiles --minid $minid
 python3 depotools.py results-survey435945.csv --json-csl > positionnements.json
+
 python3 depotools.py results-survey435945.csv --chart 'LPPR/LPR Loi de programmation de la recherche (2020)'
 python3 depotools.py results-survey435945.csv --chart 'Confinement Covid19'
 python3 depotools.py results-survey435945.csv --chart 'DUT/BUT Bachelor universitaire de technologie'
+
 python3 depotools.py results-survey435945.csv --twitter --minid $minid
 
 git add results-survey435945.csv positionnements.json *.png
